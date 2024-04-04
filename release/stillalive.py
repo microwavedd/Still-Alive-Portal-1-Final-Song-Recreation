5c1e67f6ecd52540303194367d8b017fca917950
Microondas <143319861+microwavedd@users.noreply.github.com>
2024-04-04 20:32:41 +0200

    Add files via upload

--- release/stillalive.py
+++ release/stillalive.py
@@ -3,8 +3,8 @@ import subprocess
 def execute():
     # You need to substitute the path to wherever the files "songpace.py" and "asciis.py" are in your machine.
     # More detailed tutorial in the github
-    c1 = "python C:/Users/User/Desktop/PortalStillAlive/main/songpace.py"  # Reemplaza "script1.py" con el nombre de tu primer script
-    c2 = "python C:/Users/User/Desktop/PortalStillAlive/main/asciispace.py"  # Reemplaza "script2.py" con el nombre de tu segundo script
+    c1 = "python ../Still-Alive-Portal-1-Final-Song-Recreation-main/main/songpace.py"  # Reemplaza "script1.py" con el nombre de tu primer script
+    c2 = "python ../Still-Alive-Portal-1-Final-Song-Recreation-main/main/asciispace.py"  # Reemplaza "script2.py" con el nombre de tu segundo script
     p1 = subprocess.Popen(c1, creationflags=subprocess.CREATE_NEW_CONSOLE)
     p2 = subprocess.Popen(c2, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
